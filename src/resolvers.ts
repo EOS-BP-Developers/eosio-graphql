2d97fee5d91985842f0d9832e1ef874552be2c3d
Denis Carriere <carriere.denis@gmail.com>
2018-07-25 03:01:28 -0400

    Add more types to Posts & Account

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,15 +1,68 @@
 import { MongoClient } from "mongodb";
-import { getAccount } from "eosio-mongodb-queries";
+import { getAccount, getActions } from "eosio-mongodb-queries";
 
 let client: MongoClient | null = null;
 
 // The resolvers
 export const resolvers: any = {
     Query: {
-        account: async (_: any, options: any) => {
+        account: async (_: any, options: {
+            name: string,
+            lte_block_num?: number,
+            gte_block_num?: number,
+        }) => {
             if (!client) { throw new Error("MongoClient is not initialized"); }
-            const result = await getAccount(client, options.name);
+
+            const result = await getAccount(client, options.name, options);
             return result;
+        },
+        posts: async (_: any, options: {
+            post_uuid?: string,
+            account?: string,
+            title?: string,
+            content?: string,
+            reply_to_account?: string,
+            reply_to_post_uuid?: string,
+
+            // Standard Filters
+            skip?: number,
+            limit?: number,
+            trx_id?: string,
+            block_num?: number,
+            block_id?: string,
+            lte_block_num?: number,
+            gte_block_num?: number,
+        } = {}) => {
+            if (!client) { throw new Error("MongoClient is not initialized"); }
+
+            // Default Parameters
+            const accounts = ["eosforumtest"];
+            const names = ["post"];
+            const limit = options.limit || 25; // default 25
+
+            // Optional Data Filters
+            const data = [];
+            if (options.post_uuid) { data.push({"data.post_uuid": options.post_uuid }); }
+            if (options.account) { data.push({"data.account": options.account }); }
+            if (options.title) { data.push({"data.title": options.title }); }
+            if (options.content) { data.push({"data.content": options.content }); }
+            if (options.reply_to_account) { data.push({"data.reply_to_account": options.reply_to_account }); }
+            if (options.reply_to_post_uuid) { data.push({"data.reply_to_post_uuid": options.reply_to_post_uuid }); }
+
+            // Get Actions
+            const result = await getActions(client, {
+                accounts,
+                names,
+                data,
+                limit,
+                skip: options.skip,
+                trx_id: options.trx_id,
+                block_num: options.block_num,
+                block_id: options.block_id,
+                lte_block_num: options.lte_block_num,
+                gte_block_num: options.gte_block_num,
+            });
+            return await result.toArray();
         },
     },
 };
--- src/typeDefs.ts
+++ src/typeDefs.ts
@@ -1,23 +1,88 @@
 import { gql } from "apollo-server";
 
-const account = `
-# Account Details
+const eosio = `
+# eosio account
 type Account {
-    accountName: String,
-    weight: Float,
-    ref_block_num: Int,
-    stake_net_quantity: Float,
-    stake_cpu_quantity: Float,
+    name: String!
+    block_num: Int!
+    stake_quantity: Float!
+    stake_net_quantity: Float!
+    stake_cpu_quantity: Float!
+}
+
+type Query {
+    # eosio
+    account(
+        name: String!,
+        lte_block_num: Int,
+        gte_block_num: Int,
+    ): Account
+}
+`;
+
+const actionQuery = `
+    trx_id: String,
+    block_num: Int,
+    block_id: String,
+    lte_block_num: Int,
+    gte_block_num: Int,
+    skip: Int,
+    limit: Int,
+`;
+
+const authorization = `
+type Authorization {
+    actor: String
+    permission: String
+}`;
+
+const action = `
+    trx_id: String!
+    block_num: Int!
+    block_id: String!
+    account: String!
+    name: String!
+    authorization: [Authorization]
+`;
+
+const eosioForum = `
+type PostData {
+    account: String!
+    post_uuid: String!
+    title: String!
+    content: String!
+    reply_to_account: String
+    reply_to_post_uuid: String
+    certify: Int
+    json_metadata: String
+}
+
+# eosio.forum post
+type Post {
+    ${action}
+    data: PostData!
+}
+
+extend type Query {
+    # eosio.forum
+    posts(
+        post_uuid: String,
+        account: String,
+        title: String,
+        content: String,
+        reply_to_account: String,
+        reply_to_post_uuid: String,
+        ${actionQuery}
+    ): [Post]
 }
 `;
 
 // The GraphQL schema in string form
 export const typeDefs = gql`
-    ${account}
-
-    # Query
-    type Query {
-        # Account Details
-        account(name: String): Account
+    schema {
+        query: Query
     }
+    ${authorization}
+    ${eosio}
+    ${eosioForum}
 `;
