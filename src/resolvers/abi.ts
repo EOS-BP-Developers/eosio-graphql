355469edb802b8e4e160dee2bc01014b2e24ca6b
Denis Carriere <carriere.denis@gmail.com>
2018-07-29 00:01:35 -0400

    refactor resolvers

--- src/resolvers/abi.ts
+++ src/resolvers/abi.ts
@@ -1,49 +1,50 @@
 import { getActions } from "eosio-mongodb-queries";
 import { client } from "./mongodb";
 import { abis } from "../abi";
-import { resolvers } from "./";
+
+export const abiResolvers: any = {};
 
 for (const name of Object.keys(abis)) {
-  const nameQuery = name.replace(".", "");
-  const resolver: any = {};
+    const nameQuery = name.replace(".", "");
+    const resolver: any = {};
 
-  for (const action of Object.keys(abis[name])) {
-      resolver[action] = async (options: any) => {
-          if (!client) { throw new Error("MongoClient is not initialized"); }
-          const match = [];
-          for (const key of Object.keys(options)) {
-              switch (key) {
-              case "block_id":
-              case "block_num":
-              case "trx_id":
-              case "skip":
-              case "limit":
-              case "sort":
-              case "lte_block_num":
-              case "gte_block_num":
-                  break;
-              default:
-                  const entry: any = {};
-                  entry["data." + key] = options[key];
-                  match.push(entry);
-              }
-          }
-          // TO-DO => Implement Options
-          const actions = await getActions(client, {
-              accounts: [name],
-              names: [action],
-              block_id: options.block_id,
-              block_num: options.block_num,
-              trx_id: options.trx_id,
-              skip: options.skip,
-              sort: options.sort || -1,
-              limit: options.limit || 25,
-              lte_block_num: options.lte_block_num,
-              gte_block_num: options.gte_block_num,
-              match,
-          });
-          return await actions.toArray();
-      };
-  }
-  resolvers.Query[nameQuery] = () => resolver;
+    for (const action of Object.keys(abis[name])) {
+        resolver[action] = async (options: any) => {
+            if (!client) { throw new Error("MongoClient is not initialized"); }
+            const match: any = {};
+            for (const key of Object.keys(options)) {
+                switch (key) {
+                case "block_id":
+                case "block_num":
+                case "trx_id":
+                case "skip":
+                case "limit":
+                case "sort":
+                case "contracts":
+                case "actions":
+                case "lte_block_num":
+                case "gte_block_num":
+                    break;
+                default:
+                    match["data." + key] = options[key];
+                }
+            }
+            // TO-DO => Implement Options
+            const actions = await getActions(client, {
+                accounts: options.contracts || [name],
+                names: options.actions || [action],
+                block_id: options.block_id,
+                block_num: options.block_num,
+                trx_id: options.trx_id,
+                skip: options.skip,
+                sort: options.sort || {block_num: -1},
+                limit: options.limit || 25,
+                lte_block_num: options.lte_block_num,
+                gte_block_num: options.gte_block_num,
+                match,
+            });
+            return await actions.toArray();
+        };
+    }
+    abiResolvers[nameQuery] = () => resolver;
 }
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,14 +1,12 @@
 import * as fs from "fs";
 import * as path from "path";
-import { getActions } from "eosio-mongodb-queries";
-import { client } from "./mongodb";
-import { abis } from "../abi";
+import { abiResolvers } from "./abi";
 
 // Parse package.json
 const pckg = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "..", "package.json"), "utf8"));
 
-const Query: any = {
-    // EOSIO GraphQL Metadata
+// EOSIO GraphQL App Metadata
+export const Query: any = {
     name: () => pckg.name,
     version: () => pckg.version,
     license: () => pckg.license,
@@ -17,52 +15,12 @@ const Query: any = {
     contributors: () => pckg.contributors,
 };
 
-for (const name of Object.keys(abis)) {
-  const nameQuery = name.replace(".", "");
-  const resolver: any = {};
-
-  for (const action of Object.keys(abis[name])) {
-      resolver[action] = async (options: any) => {
-          if (!client) { throw new Error("MongoClient is not initialized"); }
-          const match: any = {};
-          for (const key of Object.keys(options)) {
-              switch (key) {
-              case "block_id":
-              case "block_num":
-              case "trx_id":
-              case "skip":
-              case "limit":
-              case "sort":
-              case "contracts":
-              case "actions":
-              case "lte_block_num":
-              case "gte_block_num":
-                  break;
-              default:
-                  match["data." + key] = options[key];
-              }
-          }
-          // TO-DO => Implement Options
-          const actions = await getActions(client, {
-              accounts: options.contracts || [name],
-              names: options.actions || [action],
-              block_id: options.block_id,
-              block_num: options.block_num,
-              trx_id: options.trx_id,
-              skip: options.skip,
-              sort: options.sort || {block_num: -1},
-              limit: options.limit || 25,
-              lte_block_num: options.lte_block_num,
-              gte_block_num: options.gte_block_num,
-              match,
-          });
-          return await actions.toArray();
-      };
-  }
-  Query[nameQuery] = () => resolver;
+// Load ABI resolvers
+for (const abiResovler of Object.keys(abiResolvers)) {
+    Query[abiResovler] = abiResolvers[abiResovler];
 }
 
-// The resolvers
+// Final resolvers
 export const resolvers: any = {
     Query,
 };
