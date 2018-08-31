9cf66143896055636f2b7c86fe0aea2819ff8386
Denis Carriere <carriere.denis@gmail.com>
2018-08-31 12:15:41 -0400

    Fix linting

--- src/utils/mongoClient.ts
+++ src/utils/mongoClient.ts
@@ -1,10 +1,8 @@
 import { MongoClient } from "mongodb";
 import { MONGODB_URI } from "../../config";
 
-export let client: MongoClient | null = null;
-
 export async function getMongoClient() {
   // Intialize MongoDB Client
-  const client: MongoClient = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
-  return client
+  const client = await MongoClient.connect(MONGODB_URI, { useNewUrlParser: true });
+  return client;
 }
